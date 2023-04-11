1bed626a95b9c276a1a6c5d903df27bdbba08b09
yonatan-nyo <yonatan.njoto@gmail.com>
2023-04-11 10:03:52 +0700

    modified 1.js

--- 1.js
+++ 1.js
@@ -2,43 +2,43 @@
  * ======
  * Pacman
  * ======
- * 
+ *
  * [Desription]
  * Anda sedang bermain game pacman. Function pacman menerima input berupa
  * array yang merupakan field untuk pacman berjalan dan mengembalikan output
  * berupa array field yang telah ditelusuri oleh Pacman.
  * Disini karakter Pacman direpresentasikan sebagai 'C'.
- * 
+ *
  * Disini ada papan permainan (yang direpresentasikan sebagai array).
  * papan permainan -> ['.', '.', '*', '$']
- * 
+ *
  * - '.' merupakan jalan biasa yang dapat dilewati pacman
  * - '*' merupakan makanan
  * - '$' merupakan musuh
- * 
+ *
  * Jika pacman bertemu titik, maka dia akan berjalan melewatinya.
  * Jika pacman bertemu makanan, dia akan makan makanan tersebut dan tempat tersebut akan menjadi titik '.'
  * Jika pacman bertemu dengan '$' maka pacman akan langsung mati.
- * 
+ *
  * [Example]
  * 1.
  * @Input = ['.','*','*','*']
  * Pacman akan menelusuri papan permainan, dia akan bertemu '*' dan
  * makan makanan tersebut lalu melanjutkan perjalanan sampai index terakhir papan, maka:
  * @Output = ['.','.','.','C']
- * 
+ *
  * 2.
  * @Input = ['.', '*', '$', '*']
  * Pacman berjalan terus, tapi dia mati karena bertemu dengan '$', maka:
  * @Output = ['.', '.', '$', '*']
- * 
+ *
  * 3.
  * @Input = ['.', '*', '*', '.', '$', '*', '*']
  * Disini, ketika pacman berada pada index ke 4 dari array tersebut,
  * dia bertemu dengan sebuah monster '$'. Maka, pacman di titik tersebut langsung mati, dan
  * papan permainan tidak berubah setelah pacman menyentuh musuh tersebut, sehingga:
  * @Output = ['.', '.', '.', '.' , '$', '*', '*']
- * 
+ *
  * [Rules]
  * 1. `Wajib menuliskan pseudocode` atau -50
  * 2. Dilarang menggunakan iterators (forEach, map, filter, reduce)
@@ -47,18 +47,19 @@
 
 function pacman(array) {
   // Write your code here
+  //ubah
 }
-
-console.log(pacman(['*', '.', '.', '*']));
+console.log("adads");
+console.log(pacman(["*", ".", ".", "*"]));
 // ['.', '.', '.', 'C']
 
-console.log(pacman(['.', '*', '$', '*']));
+console.log(pacman([".", "*", "$", "*"]));
 //['.', '.', '$', '*']
 
-console.log(pacman(['.', '.', '.', '.', '*', '*', '*', '*']));
+console.log(pacman([".", ".", ".", ".", "*", "*", "*", "*"]));
 //['.', '.', '.', '.', '.', '.', '.', 'C']
 
-console.log(pacman(['.', '*', '*', '.', '$', '*', '*']));
+console.log(pacman([".", "*", "*", ".", "$", "*", "*"]));
 //['.', '.', '.', '.' , '$', '*', '*']
 
 console.log(pacman([]));
